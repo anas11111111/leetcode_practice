2c7167ef1363d3bc5b807d5bf9db42e6b3608717
anas11111111 <160204021@aust.edu>
2022-02-11 23:12:40 +0600

    commit

--- insert_node_beginning_linklist.py
+++ insert_node_beginning_linklist.py
@@ -3,7 +3,6 @@ class Node:
         self.data = data
         self.next = None
 
-
 class Linklist:
     def __init__(self, li) -> None:
         self.head = Node(None)
@@ -11,42 +10,64 @@ class Linklist:
         self.li = li
 
     def traverse_linklist_array(self):
-       
         li = self.li
         temp = None
         for i in li:
             n = Node(i)
             if temp is None:
                 temp = n
-                print(temp.data)
                 self.head = n
             else:
-                print(n.data)
                 temp.next = n
                 temp = temp.next
-
         self.tail = temp
 
     def node_at_begin(self, newData):
         newNode = Node(newData)
         newNode.next = self.head
         self.head = newNode
-        print(self.head,newNode.next.data)
-        print()
+    
+    def node_at_end(self,newData):
+        newNode = Node(newData)
+        self.tail.next = newNode
 
-    def print_linklist_after_insertion(self):
+    def node_at_after_existing_node(self,newData,existingData):
+        runner = self.head
+        newNode = Node(newData)
+        existingNode = Node(existingData)
+        while runner is not None:
+            if runner.data is existingNode.data:
+                newNode.next = runner.next
+                runner.next = newNode
+                break
+            
+            else:
+                runner = runner.next
+
+    def print_linklist(self):
         runner = self.head
         while runner is not None:
             print(runner.data)
-            runner = runner.next  
+            runner = runner.next
+    
+    
 
+
+#######################################
 
 li = [2, 3, 4, 5]
 obj = Linklist(li)
 obj.traverse_linklist_array()
+obj.print_linklist()
 print()
 obj.node_at_begin(1)
-obj.print_linklist_after_insertion()
+obj.print_linklist()
+print()
+obj.node_at_end(6)
+obj.print_linklist()
+print()
+obj.node_at_after_existing_node(7,3)
+obj.print_linklist()
 
 
 # let assume I want to add a value 1 at the beginning position of the linklist
