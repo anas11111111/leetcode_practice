c419cb87f8a6f29559cc94cd9fd8f21d85084df2
anassikder <anas@uxd.co.jp>
2022-02-16 12:56:18 +0600

    commit

---
+++ 29_Divide Two Integers.py
@@ -0,0 +1,25 @@
+def divide(dividend, divisor):
+    MAX_VALUE = 2**31 - 1
+    MIN_VALUE = -2**31
+
+    if divisor == 0 or (divisor == -1 and dividend == MIN_VALUE):
+        return MAX_VALUE
+
+    q = 0
+    sign = 1
+    if (dividend > 0 and divisor < 0) or (dividend < 0 and divisor > 0):
+        sign = -1
+
+    while dividend >= divisor:
+        shift_l = 0
+        while dividend >= (divisor << shift_l):
+            shift_l = shift_l + 1
+
+        q = q + (1 << (shift_l-1))
+        dividend = dividend - (divisor << (shift_l-1))
+
+    return -q if sign == -1 else q
+
+
+res = divide(10, 3)
+print(res)
